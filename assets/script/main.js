a4d6108d491c9019fc29753f6773c1ac307e679f
shin <shin.nkt.09@gmail.com>
2025-04-02 23:32:24 +0900

    head情報追記、選べる3つの理由〜費用レイアウト作成、コース・目的のタブ切り替え機能実装

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,1 +1,11 @@
-  
+$(document).ready(function () {
+    $('.choose__tab').click(function () {
+        let tabId = $(this).attr('id');
+
+        $('.choose__tab').removeClass('choose__tab--active');
+        $(this).addClass('choose__tab--active');
+
+        $('.choose__content').removeClass('choose__content--active')
+        $('#content-' + tabId).addClass('choose__content--active');
+    });
+});
