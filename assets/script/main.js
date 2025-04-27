5afc6b3271f0cb37d53ba19bd0881a74482a2df3
shin <shin.nkt.09@gmail.com>
2025-04-27 20:48:46 +0900

    レビュー指摘事項修正

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,19 +1,23 @@
 $(document).ready(function () {
+    // スマホハンバーガーメニュークリックイベント
     $('.hamburger').click(function () {
+        // ハンバーガーメニューを表示する
         $('.sp-nav').toggleClass('active');
     });
-
-    $('.choose__tab').click(function () {
-        let tabId = $(this).attr('id');
-
-        $('.choose__tab').removeClass('choose__tab--active');
-        $(this).addClass('choose__tab--active');
-
-        $('.choose__content').removeClass('choose__content--active')
-        $('#content-' + tabId).addClass('choose__content--active');
-    });
-
+    // スマホハンバーガーメニュー内リンククリックイベント
     $('.sp-nav__item').click(() => {
+        // ハンバーガーメニューを非表示にする
         $('.sp-nav').removeClass('active');
     })
+
+    // コース・目的エリアタブクリックイベント
+    $('.choose__tab').click(function () {
+        let tabId = $(this).attr('id');
+        // 表示中のタブを非表示にする
+        $('.choose__tab').removeClass('choose__tab--active');
+        $('.choose__content').removeClass('choose__content--active')
+        // クリックしたタブを表示する
+        $(this).addClass('choose__tab--active');
+        $('#content-' + tabId).addClass('choose__content--active');
+    });
 });
